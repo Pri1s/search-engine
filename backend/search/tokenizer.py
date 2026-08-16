cc3c1d1949ad0f51a25ab4d3e60bfb119d561569
Priyansu Biswal <pri@Priyansus-MacBook-Pro.local>
2026-08-16 12:31:17 -0500

    test inverted index creation from docs in db

--- backend/search/tokenizer.py
+++ backend/search/tokenizer.py
@@ -1,6 +1,6 @@
 import re # for working with RegEx
 
-def tokenizer(text: str):
+def tokenize(text: str):
     text = text.lower()
     words = re.findall(r"\b[a-z]+\b", text) # find complete words made of only lowercase letter
     return words # returns an array of words
